500b1ba3d3af7d63d10563fd6819d70703adb716
Ilyak16 <KupriyanovIN@mer.ci.nsu.ru>
2023-10-13 23:44:02 +0700

    Game
    Game

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+from Window import Window
+import pygame
+pygame.init()
+icon = pygame.image.load("images/icon.png")
+pygame.display.set_icon(icon)
+win = Window.window(1240, 900)
+win = pygame.display.set_mode((win.width, win.height))
+pygame.display.set_caption("RTS_GAME")
+clock = pygame.time.Clock()
+while True:
+    win.fill((0, 100, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    clock.tick(60)
+    pygame.display.update()
+
