1ad9028aa9c564ad5ca18df164c05cda75f14c4b
Ilyak16 <KupriyanovIN@mer.ci.nsu.ru>
2023-12-01 23:30:23 +0700

    Reboot

--- main.py
+++ main.py
@@ -8,75 +8,28 @@ def Quit():
     pygame.quit()
     sys.exit()
 
-def Play():
-    pygame.init()
-    window.fill((255, 255, 255))
-    res = (400, 400)
-    screen = pygame.transform.scale(window, res)
-    pygame.transform.scale(window, res)
-    title_rasmer = 16
-    chank_rasmer = 8
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-        window.blit(pygame.transform.scale(screen, res), (0, 0))
-        clock.tick(60)
-        pygame.display.update()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 pygame.init()
-pygame.display.set_caption("RTS_GAME")
+pygame.display.set_caption("GAME")
 icon = pygame.image.load("images/icon.png")
 pre_fon = pygame.image.load("images/Заставка.png")
-nas = pygame.image.load("images/RTS_GAME.png")
-nas.set_colorkey('White')
+nas = pygame.image.load("images/COSMO.png")
+nas.set_colorkey('Black')
 mest_nas = nas.get_rect(center=(620, 150))
 pygame.display.set_icon(icon)
 Width = 1240
 Height = 900
 clock = pygame.time.Clock()
 window = pygame.display.set_mode((Width, Height))
-
-
 window.blit(nas, mest_nas)
 button_PLAY = Button.ImageButton(Width/2 - (250/2), 300, 250, 75, "PLAY", "images/Кнопка_до.png", "images/Кнопка_после.png")#, "Sounds/Klick.mp3"
 button_QUIT = Button.ImageButton(Width/2 - (250/2), 400, 250, 75, "QUIT", "images/Кнопка_до.png", "images/Кнопка_после.png")#, "Sounds/Klick.mp3"
 pygame.display.update()
+def Play():
+    Fon = pygame.image.load("images/Fon.png")
+    window.blit(Fon, (0, 0))
+
+
+
 def main_menu():
     running = True
     while running:
