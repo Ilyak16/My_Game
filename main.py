8398acd168c1631f2de128dddf8248312137dab2
Ilyak16 <kupriyanovin@mer.ci.nsu.ru>
2023-12-02 14:15:52 +0700

    uwu

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import pygame
 import sys
 
 import Button
+
 from Button import ImageButton
 def Quit():
     pygame.quit()
     sys.exit()
+
+class Player:
+    def __init__(self, x, y, width, height, model, HP):
+        self.player_x = x
+        self.player_y = y
+        self.width_player = width
+        self.height_player = height
+        self.model = pygame.transform.scale(pygame.image.load(model), (width, height))
+        self.HP = HP
 
 pygame.init()
 pygame.display.set_caption("GAME")
@@ -14,6 +24,7 @@ icon = pygame.image.load("images/icon.png")
 pre_fon = pygame.image.load("images/Заставка.png")
 nas = pygame.image.load("images/COSMO.png")
 nas.set_colorkey('Black')
+Player = Player(940, 0, 300, 150, "images/Player.png", 3)
 mest_nas = nas.get_rect(center=(620, 150))
 pygame.display.set_icon(icon)
 Width = 1240
@@ -25,8 +36,17 @@ button_PLAY = Button.ImageButton(Width/2 - (250/2), 300, 250, 75, "PLAY", "image
 button_QUIT = Button.ImageButton(Width/2 - (250/2), 400, 250, 75, "QUIT", "images/Кнопка_до.png", "images/Кнопка_после.png")#, "Sounds/Klick.mp3"
 pygame.display.update()
 def Play():
+    clock = pygame.time.Clock()
     Fon = pygame.image.load("images/Fon.png")
     window.blit(Fon, (0, 0))
+
+
+
+    while True:
+
+
+
+        clock.tick(60)
 
 
 
