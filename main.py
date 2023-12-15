df2a8af31e8ee9e39f5a3a3f9b4f6bbd59cd8097
KupriyanovIN <kupriyanovin@mer.ci.nsu.ru>
2023-12-15 15:37:33 +0700

    Коментарии

--- main.py
+++ main.py
@@ -29,41 +29,49 @@ class Enemy:
         self.HP = HP
         self.speed = speed
 pygame.init()
+# Создание иконки, заднего фона, названия и его обработка
 pygame.display.set_caption("GAME")
 icon = pygame.image.load("images/icon.png")
 pre_fon = pygame.image.load("images/Заставка.png")
 nas = pygame.image.load("images/COSMO.png")
+mest_nas = nas.get_rect(center=(620, 150))
 nas.set_colorkey('Black')
+lose = pygame.font.Font("Fonts/bionicle-training-card-font-2-4.ttf", 40)
+lose_name = lose.render("You lose!", False, (255, 255, 255))
+# Создание игрока с помощью класса
 gamer = Player(495, 660, 250, 240, "images/Player.png", 3, 10)
+#  Создание пули и списка под них
 bullet = pygame.transform.scale(pygame.image.load("images/Shot2.png"), (70, 70))
 bullets = []
+# Создание врагов с помощью класса и списка под них
 enemy = Enemy(0, 0, 150, 167, "images/Enemy.png", 1, 5)
 enemies = []
-mest_nas = nas.get_rect(center=(620, 150))
+# Установка иконки, ширины и высоты экрана, ограничение фпс, названия
 pygame.display.set_icon(icon)
 Width = 1240
 Height = 900
 clock = pygame.time.Clock()
 window = pygame.display.set_mode((Width, Height))
 window.blit(nas, mest_nas)
-lose = pygame.font.Font("Fonts/bionicle-training-card-font-2-4.ttf", 40)
-lose_name = lose.render("You lose!", False,(255, 255, 255))
+# Создание кнопок с помощью класса
 button_PLAY = Button.ImageButton(Width/2 - (250/2), 300, 250, 75, "PLAY", "images/Кнопка_до.png", "images/Кнопка_после.png")#, "Sounds/Klick.mp3"
 button_QUIT = Button.ImageButton(Width/2 - (250/2), 400, 250, 75, "QUIT", "images/Кнопка_до.png", "images/Кнопка_после.png")#, "Sounds/Klick.mp3"
-pygame.display.update()
+# Создаём функцию игры
 def Play():
+    # Создание фона, таймера и ограничителя фпс и задавание здоровья игрока
     clock = pygame.time.Clock()
     Fon = pygame.image.load("images/Fon.png")
-    window.blit(Fon, (0, 0))
     spawn_timer = pygame.USEREVENT + 1
     pygame.time.set_timer(spawn_timer, 3500)
     gamer.HP = 3
     gaming = True
     while gaming:
+        # Вывод фона, отрисовка игрока, нахождение рандом места спавна, наделение игрока моделькой
         window.blit(Fon, (0, 0))
         window.blit(gamer.model, (gamer.player_x, gamer.player_y))
         enemy.enemy_x = random.randint(0, 1090)
         gamer_rect = gamer.model.get_rect(topleft=(gamer.player_x, gamer.player_y))
+        # перебор врагов в списке
         for (e, el) in enumerate(enemies):
             window.blit(enemy.model, (el.x, el.y))
             el.y += enemy.speed
@@ -71,17 +79,19 @@ def Play():
                 gamer.HP = 0
             if el.y > Height:
                 enemies.pop(e)
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_a] and gamer.player_x > 0:
-            gamer.player_x -= gamer.speed
-        elif keys[pygame.K_d] and gamer.player_x < Width - gamer.width_player:
-            gamer.player_x += gamer.speed
+        # Перебор пуль в списке
         if bullets:
             for (i, el) in enumerate(bullets):
                 window.blit(bullet, (el.x, el.y))
                 el.y -= 10
                 if el.y < -70:
                     bullets.pop(i)
+        # Проверка на действия игрока
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and gamer.player_x > 0:
+            gamer.player_x -= gamer.speed
+        elif keys[pygame.K_d] and gamer.player_x < Width - gamer.width_player:
+            gamer.player_x += gamer.speed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -90,6 +100,7 @@ def Play():
                 bullets.append(bullet.get_rect(topleft=(gamer.player_x + 90, gamer.player_y - 30)))
             if event.type == spawn_timer:
                 enemies.append(enemy.model.get_rect(topleft=(enemy.enemy_x, enemy.enemy_y)))
+        # Проверка условий проигрыша
         if gamer.HP == 0:
             enemies.clear()
             bullets.clear()
@@ -102,7 +113,10 @@ def Play():
 def main_menu():
     running = True
     while running:
+        # Обновление экрана
         window.blit(pre_fon, (0, 0))
+        window.blit(nas, mest_nas)
+        # Проверка на действия игрока
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 Quit()
@@ -112,10 +126,10 @@ def main_menu():
                 Play()
             for btn in [button_QUIT, button_PLAY]:
                 btn.click(event)
+        # Проверка наведённости
         for btn in [button_QUIT, button_PLAY]:
             btn.check_hover(pygame.mouse.get_pos())
             btn.Draw(window)
-        window.blit(nas, mest_nas)
         pygame.display.flip()
 main_menu()
 
